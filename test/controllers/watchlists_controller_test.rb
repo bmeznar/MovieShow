0ef1c51dfd23d707860ccdec04ca85d97f381f48
Ales717 <prosenjak.ales77@gmail.com>
2021-04-21 08:41:15 +0200

    test

--- test/controllers/watchlists_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class WatchlistsControllerTest < ActionDispatch::IntegrationTest
-  test "should get update" do
-    get watchlists_update_url
-    assert_response :success
-  end
-end
