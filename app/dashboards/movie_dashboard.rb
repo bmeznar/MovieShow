fd1e0887537db40a1f733790c3bddfb2eac03980
Ales717 <prosenjak.ales77@gmail.com>
2021-04-21 09:44:48 +0200

    test

--- app/dashboards/movie_dashboard.rb
+++ app/dashboards/movie_dashboard.rb
@@ -10,6 +10,7 @@ class MovieDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     id: Field::Number,
     title: Field::String,
+    created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }.freeze
 
@@ -19,24 +20,26 @@ class MovieDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    id
-    title
-    updated_at
+    :id
+    :title
+    :created_at
+    :updated_at
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-    id
-    tilte
-    updated_at
+    :id
+    :title
+    :created_at
+    :updated_at
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-
+    :title
   ].freeze
 
   # COLLECTION_FILTERS
