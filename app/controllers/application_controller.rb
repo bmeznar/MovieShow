a221e2149ca9e8eda829a7cbb6be03c61f20e9d9
bmeznar <blaz.meznar.bm@gmail.com>
2021-05-05 09:39:19 +0200

    fixing stripe pt4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  if user_signed_in?
+    if current_user.subscribed!=true?
+      redirect_to '/subscribers/new'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       root to: "users#index"
     end
   resources :subscribers
+  put 'subscriber_update' => 'subscribers#update'
   devise_for :users, :controllers => { :registrations => "registrations" }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
