f1392516267f2075c8517bbf7188c6382d0513f6
bmeznar <70625118+bmeznar@users.noreply.github.com>
2021-05-12 09:10:21 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
     def watchlist_text
         return @watchlist_exists ? "Remove from Watchlist" : "Add to Watchlist"
     end
+    
+    def check_user
+        if user_signed_in?
+          if !current_user.subscribed
+            redirect_to new_subscriber_path
+          end
+        end
+    end
 
     helper_method :watchlist_text
 end
